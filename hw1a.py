4e624cab01091d07c6c7885c3a17d90a09351f1c
tuckerbrooks <32171110+tuckerbrooks@users.noreply.github.com>
2019-09-14 16:45:35 -0400

    Create hw1a.py

---
+++ hw1a.py
@@ -0,0 +1,13 @@
+# File: hw1a.py
+# Author: Tucker Brooks
+# Date: September 14, 2019
+# Section: 1001
+# E-mail: tucker.brooks@maine.edu
+# Description:
+# Asks user for their name, and then suggests getting ice cream
+# Collaboration:
+# N/A
+
+name = input("Hi, friend! What's your name?")
+print ("Hi,", name, ", I'm happy to meet you.")
+print ("Let's go get some ice cream.")
